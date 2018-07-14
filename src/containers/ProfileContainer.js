f8fb5aa4c433e1d943a261bf37d671eeb108ab67
Angela Roscel Almoro <aralmoro@gmail.com>
2018-07-15 01:53:54 +0800

    Added image on profile

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { StyleProvider, Container, Button, Text } from 'native-base';
+import { View, Image } from 'react-native';
+import { StyleProvider, Container, Button, Text, Thumbnail } from 'native-base';
 import AppHeader from '../components/AppHeader';
 
 import getTheme from '../../native-base-theme/components';
 import commonColor from '../../native-base-theme/variables/commonColor';
+
+const uri = "https://freeiconshop.com/wp-content/uploads/edd/person-girl-flat.png";
 
 class Profile extends Component {
   render() {
@@ -13,6 +15,7 @@ class Profile extends Component {
         <Container style={{ backgroundColor: '#B5E9E5' }}>
           <AppHeader headerTitle='Profile' />
           <View style={{ flex: 0, flexDirection: 'column', alignItems: 'center' }}>
+            <Thumbnail source={{ uri }} style={{ height: 70, width: 70, marginTop: 50 }} />
             <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Stella Summers</Text>
             <Button transparent style={{ alignSelf: 'center' }}
               onPress={() => this.props.navigation.navigate('Rewards')}>
